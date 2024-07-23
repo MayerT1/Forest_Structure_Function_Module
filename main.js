7dcaaca9030108f094cb2ba802ceee6b0dbb7882
Tim Mayer <45499910+MayerT1@users.noreply.github.com>
2024-07-23 12:10:34 -0500

    7/23/24 Dev push
    7/23/24 Dev push

---
+++ main.js
@@ -0,0 +1,17 @@
+var routine = require("users/tjm0042/PHD:Forest_Structure_Function_Module/routine.js");
+var indices = require("users/tjm0042/PHD:Forest_Structure_Function_Module/indices.js");
+var s1Correction = require("users/tjm0042/PHD:Forest_Structure_Function_Module/s1_correction.js");
+var model = require("users/tjm0042/PHD:Forest_Structure_Function_Module/model.js");
+// var modelTuner = require("users/biplov/bhutan-aces-v-1:model_tuner.js");
+var utils = require("users/tjm0042/PHD:Forest_Structure_Function_Module/utils.js");
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+exports.routine = routine;
+exports.indices = indices;
+exports.s1Correction = s1Correction;
+exports.model = model;
+// // exports.modelTuner = modelTuner;
+exports.utils = utils;
